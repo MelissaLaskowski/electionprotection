30b2b90a695c403226d7365731288fc09f38207b
Melissa Laskowski <melissalaskowski@Melissas-MacBook-Pro.local>
2019-02-02 12:22:11 -0500

    added in the template pages

--- views.py
+++ views.py
@@ -4,4 +4,14 @@ from _init_ import app
 @app.route('/')
 def landing_page():
 	#default landing page
-	return "let's get voting!"
+	return render_template("landing.html")
+
+@app.route('/vote')
+def vote_page():
+	#voter page endpoint
+	return render_template('vote.html')
+
+@app.route('/results')
+def results_page():
+
+	return render_template('results.html')
