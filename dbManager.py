ac36b91b3a41226f2226bfb8dcc8e38efb3f7685
melissalaskowski <mellaskowski12@gmail.com>
2019-04-04 22:27:25 -0400

    remove unneccessay function

--- dbManager.py
+++ dbManager.py
@@ -21,36 +21,6 @@ def close_connection(connection):
 	connection.close()
 	return
 
-def register():
-	#this is separate from run query becuase it does not fetch anything, just insert
-	connection = open_connection()
-	cursor = connection.cursor()
-	voterReader = csv.reader(open('DB/voter_data.csv', newline=''))
-	line_num = 0
-	for row in voterReader:
-		#do this check to avoid the headers to the csv file
-		if line_num != 0:
-			#the hash is generated from the ssn and dob
-			password = generate_password_hash(row[1]+row[2])
-			name =""; 
-			if "'" in row[0]:
-				for char in row[0]:
-					if char== "'":
-						name+="''"
-					else:
-						name+=char
-			else:
-				name = row[0]
-			values = (" '"+name+"' , '"+password+"' , '"+row[3]+"' ")
-			query =("INSERT INTO authorized_voters VALUES ( "+ values + ");")
-			print(query)
-
-			cursor.execute(query)
-			connection.commit()
-		else:
-			line_num+=1
-	close_connection(connection)
-
 #after a user has voted, mark them as voted
 def mark_voted(fullName):
 	connection = open_connection()
